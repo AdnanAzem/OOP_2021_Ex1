f1c946376048d8d9f483b38b3f66958fb966792e
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-15 12:28:17 +0200

    Adding Elevator.py

---
+++ elevator.py
@@ -0,0 +1,21 @@
+import json
+
+class Elevator:
+
+    def __init__(self, id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time):
+        self.id = id
+        self.speed = speed
+        self.min_floor = min_floor
+        self.max_floor = max_floor
+        self.close_time = close_time
+        self.open_time = open_time
+        self.start_time = start_time
+        self.stop_time = stop_time
+
+    def __str__(self) -> str:
+        return f"id:{self.id} ,speed:{self.speed} ,minF:{self.min_floor}" \
+               f" ,maxF:{self.max_floor} ,closeT:{self.close_time} " \
+               f",openT:{self.open_time} ,startT:{self.start_time} ,stopT:{self.stop_time}\n"
+
+    def __repr__(self) -> str:
+        return self.__str__()
