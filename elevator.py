3a8877d3a12b1fae27d7f24d3e42f83c89bf1361
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-17 17:31:18 +0200

    updating elevator class

--- elevator.py
+++ elevator.py
@@ -1,23 +1,53 @@
-import json
 
 class Elevator:
 
-    def __init__(self, id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time):
-        self.id = id
-        self.speed = speed
-        self.min_floor = min_floor
-        self.max_floor = max_floor
-        self.close_time = close_time
-        self.open_time = open_time
-        self.start_time = start_time
-        self.stop_time = stop_time
+    def __init__(self,file):
+        self.id = int(file["_id"])
+        self.speed = float(file["_speed"])
+        self.minFloor = int(file["_minFloor"])
+        self.maxFloor = int(file["_maxFloor"])
+        self.closeTime = float(file["_closeTime"])
+        self.openTime = float(file["_openTime"])
+        self.startTime = float(file["_startTime"])
+        self.stopTime = float(file["_stopTime"])
+        self.listCalls = []
+        self.elevPos = 0
 
-    def __str__(self) -> str:
-        return f"id:{self.id} ,speed:{self.speed} ,minF:{self.min_floor}" \
-               f" ,maxF:{self.max_floor} ,closeT:{self.close_time} " \
-               f",openT:{self.open_time} ,startT:{self.start_time} ,stopT:{self.stop_time}\n"
+    def __str__(self):
+        return " Elevator ID: " + str(self.id) + "\n" + "Speed: " + str(self.speed) + " CloseTime: " + str(self.closeTime)\
+        + " OpenTime: " + str(self.openTime) + " StartTime: " + str(self.startTime) + " StopTime: " + str(self.stopTime) + "\n"
 
-    def __repr__(self) -> str:
-        return self.__str__()
+    def doneCalls(self,time):
+        for c in self.listCalls:
+            if c.totalTime < time:
+                c.changeState(c.DONE)
+                self.listCalls.remove(c)
+                self.elevPos = c.dest
+
+    def changePosition(self,i):
+        if i < self.minFloor or i > self.maxFloor:
+            return
+        self.elevPos = i
+
+    def calcTime(self, currElevPos, dest) -> float:
+        floors = abs(currElevPos - dest)
+        totalTime = self.openTime + self.closeTime + self.startTime + self.stopTime
+        return floors / self.speed + totalTime
+
+    def arrivedTime(self,call):
+        totalTime = 0
+        currElevPos = self.elevPos
+        for c in self.listCalls:
+            totalTime += self.calcTime(currElevPos,c.src)
+            currElevPos = c.src
+        if len(self.listCalls) != 0:
+            totalTime += self.calcTime(currElevPos,self.listCalls[-1].dest)
+            currElevPos = self.listCalls[-1].dest
+        totalTime += float(self.calcTime(currElevPos,call.src))
+        return totalTime
+
+    def addCallToList(self, call):
+        self.listCalls.append(call)
+
     
     
